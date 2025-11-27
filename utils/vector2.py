0e96c224a03d92ebf2e75508c62b5f2c8f4ace97
Myson Dio <sendorfbg@gmail.com>
2025-11-27 17:15:35 -0300

    refactor: using isinstance instead of type(var) when checking arguments
    also removed __dict__ from the Vector2 class

--- utils/vector2.py
+++ utils/vector2.py
@@ -4,10 +4,14 @@ _T = TypeVar("_T", int, float)
 
 class Vector2(Generic[_T]):
     
+    __slots__ = ['x', 'y', '__type']
     class ArgumentException(Exception):
         def __init__(self, message: str, code: int):
             super().__init__(message)
+            self.msg = message
             self.code: int = code
+        def __str__(self) -> str:
+            return f"\033[1;31mArgumentException[{self.code}]: {self.msg}[\033[0m"
     
     def __init__(self, x: _T, y: _T):
         self.__check_args(x, y)
@@ -16,18 +20,17 @@ class Vector2(Generic[_T]):
         self.__type: str = "" 
         
     def __get_type(self):
-        if type(self.x) is float or type(self.y) is float:
+        if isinstance(self.x, float) or isinstance(self.y, float):
             self.__type = "f"
         else:
             self.__type = "i"
             
     def __check_args(self, x:_T, y:_T):
-        if type(x) is not int and type(x) is not float:
+        if not isinstance(x, (int, float)):
             raise self.ArgumentException(f"\033[1;31m[ERROR] - Argument x is of type: {type(x)}. Allowed types are: float and int\033[0m", 401)
         else:
-            if type(y) is not int and type(y) is not float:
+            if not isinstance(y, (int, float)):
                 raise self.ArgumentException(f"\033[1;31m[ERROR] - Argument y is of type: {type(y)}. Allowed types are: float and int\033[0m", 401)
-            
             
     def __repr__(self):
         self.__get_type()
